dfa93af5bf1fce562941ce0a37d5b6223f24a2bd
max.rodionov <max.rodionov@synesis.ru>
2015-02-17 11:40:17 +0300

    more verbose install, requirements.txt added

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-set -x
+set -x  # Print a trace of simple commands and their arguments
 install_dir="$HOME/bin"
 mkdir -p ${install_dir}
 install ./toru ${install_dir}
 install ./toen ${install_dir}
 install ./t2.py ${install_dir}
 set +x
-echo "t2 successfully installed :) ! Check it with \"toru hello\""
+
+echo "> toru hello"
+toru hello
+ret=$?
+
+if [ "$ret" != "10" ]; then
+    echo "Something gone wrong, check for pip dependency installed by: pip install -r requirements.txt"
+else
+    echo "t2 successfully installed. Check it with \"toru hello\""
+fi
+
